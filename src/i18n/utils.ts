abf86ba3588d21d768eb29493ae0a5b2f05ae56a
checomendoza <ezequiel.mendoza@gmail.com>
2024-05-10 07:47:07 -0300

    Add Footer

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -3,7 +3,14 @@
 import type { TypesLang } from './i18n.types'
 import { ui, defaultLang } from './ui'
 export const useTranslation = (lang: TypesLang) => {
-	return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-		return ui[lang][key] || ui[defaultLang][key]
+	return function t(key: keyof (typeof ui)[typeof defaultLang], params?: Record<string, string>) {
+		let translation = ui[lang][key] || ui[defaultLang][key]
+		if (params) {
+			Object.keys(params).forEach((paramKey) => {
+				const paramValue = params[paramKey]
+				translation = translation.replace(`{{${paramKey}}}`, paramValue)
+			})
+		}
+		return translation
 	}
 }
