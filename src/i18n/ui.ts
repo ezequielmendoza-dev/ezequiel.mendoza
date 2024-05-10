6a6c94cbd642af699ae1fd2a9e844064fbb76522
checomendoza <ezequiel.mendoza@gmail.com>
2024-05-10 07:13:52 -0300

    Add Cards Projects & Logos

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,15 +11,16 @@ export const ui = {
 		greeting: 'Hola! Me llamo',
 		description:
 			'Soy un apasionado desarrollador de software frontend con más de 15 años de experiencia en la industria. Soy de Corrientes, Argentina. Mi enfoque principal ha sido siempre crear experiencias de usuario excepcionales a través de interfaces atractivas y funcionales. He trabajado con una variedad de tecnologías y herramientas, incluyendo React, React Native, Next.js, Tailwind CSS y Material UI, lo que me ha permitido desarrollar aplicaciones web y móviles innovadoras',
+		toolsTitle: 'Herramientas y Habilidades',
+		projectTitle: 'Proyectos',
 	},
-	tools: 'Herramientas y Habilidades',
-	projects: 'Proyectos',
+
 	en: {
 		title: 'Software Developer',
 		greeting: 'Hi! My name is',
 		description:
 			'I am a passionate frontend software developer with over 15 years of experience in the industry. I am from Corrientes, Argentina. My primary focus has always been to create exceptional user experiences through appealing and functional interfaces. I have worked with a variety of technologies and tools, including React, React Native, Next.js, Tailwind CSS, and Material UI, allowing me to develop innovative web and mobile applications.',
-		tools: 'Tools & Skills',
-		projects: 'Projects',
+		toolsTitle: 'Tools & Skills',
+		projectTitle: 'Projects',
 	},
 }
