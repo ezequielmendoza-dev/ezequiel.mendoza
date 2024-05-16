b6d2e874c234132174f3233a2d6f810566080519
checomendoza <ezequiel.mendoza@gmail.com>
2024-05-16 09:25:47 -0300

    Replace icons from tools and skills

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -16,13 +16,35 @@ export const Experiences: TypesExperiencesTranslates = {
 		},
 		{
 			name: 'Desarrollador Freelance',
-			period: '2008-actual',
+			period: '2008 - actual',
 			description: 'Desarrollador fullstack y mobile en diversos proyectos para clientes de diferentes industrias',
 		},
 		{
 			name: 'Globant',
 			period: '2021 - actual',
 			description: 'Software Developer Ssr Adv. Desarrollo web frontend en diferentes proyectos',
+		},
+	],
+	en: [
+		{
+			name: 'Itemax',
+			period: '2004 - 2006',
+			description: 'Technical support and attention to internet service customers',
+		},
+		{
+			name: 'Banco de Corrientes',
+			period: '2006 - 2021',
+			description: 'Commercial and operational attention to banking clients',
+		},
+		{
+			name: 'Freelance Developer',
+			period: '2008 - current',
+			description: 'Fullstack and mobile developer in various projects for clients from different industries',
+		},
+		{
+			name: 'Globant',
+			period: '2021 - current',
+			description: 'Software Developer Ssr Adv. Frontend web development in different projects',
 		},
 	],
 }
--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,32 +1,6 @@
 /** @format */
 
-// TECH: React | ReactNative | Next.js | StyledComponents | MUI | Tailwind | Javasript | Typscript | NativeBase
 import { wordsToHighLight } from '../constants'
-export const getColorsByTech = (val: string) => {
-	switch (val.toLowerCase()) {
-		case 'react':
-			return 'bg-cyan-600 text-cyan-100'
-		case 'react native':
-			return 'bg-cyan-700 text-cyan-100'
-		case 'next.js':
-			return 'bg-gray-900 text-gray-200'
-		case 'tailwind':
-			return 'bg-green-900 text-green-200'
-		case 'mui':
-			return 'bg-blue-900 text-blue-200'
-		case 'styled components':
-			return 'bg-orange-300 text-orange-800'
-		case 'native base':
-			return 'bg-purple-900 text-purple-200'
-		case 'javascript':
-			return 'bg-yellow-600 text-yellow-100'
-		case 'typescript':
-			return 'bg-blue-700 text-blue-100'
-		default:
-			return 'bg-gray-200 text-gray-700'
-	}
-}
-
 export const highlightWord = (text: string) => {
 	const escapedWords = wordsToHighLight.map((wordObj) => wordObj.word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'))
 	const regex = new RegExp(`(${escapedWords.join('|')})`, 'gi')
