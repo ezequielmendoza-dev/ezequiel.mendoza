940a6af1631c25bc10bedb1bf68adb57ea127b48
checomendoza <ezequiel.mendoza@gmail.com>
2024-05-20 19:09:13 -0300

    Fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,19 @@
 import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
-
+const SERVER_PORT = 4321
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`
+const PRODUCTION_URL = 'https://checomendoza.github.io/ezequiel.mendoza'
+let BASE_URL = LOCALHOST_URL
+const SCRIPT = process.env.npm_lifecycle_script || ''
+const isBuild = SCRIPT.includes('astro build')
+if (isBuild) {
+	BASE_URL = PRODUCTION_URL
+}
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://checomendoza.github.io',
-	base: '/ezequiel.mendoza',
+	site: BASE_URL,
+	// base: '/ezequiel.mendoza',
 	integrations: [tailwind()],
 	i18n: {
 		defaultLocale: 'es',
