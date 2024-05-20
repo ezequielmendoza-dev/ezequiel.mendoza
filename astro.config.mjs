143bb8cf90eb6ae1d899948685db9c3dd3017084
checomendoza <ezequiel.mendoza@gmail.com>
2024-05-20 19:14:10 -0300

    Add base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,20 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 const SERVER_PORT = 4321
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`
-const PRODUCTION_URL = 'https://checomendoza.github.io/ezequiel.mendoza'
-let BASE_URL = LOCALHOST_URL
+const PRODUCTION_URL = 'https://checomendoza.github.io'
+let SITE_URL = LOCALHOST_URL
+let BASE_URL = ''
+
 const SCRIPT = process.env.npm_lifecycle_script || ''
 const isBuild = SCRIPT.includes('astro build')
 if (isBuild) {
-	BASE_URL = PRODUCTION_URL
+	SITE_URL = PRODUCTION_URL
+	BASE_URL = '/ezequiel.mendoza'
 }
 // https://astro.build/config
 export default defineConfig({
-	site: BASE_URL,
-	// base: '/ezequiel.mendoza',
+	site: SITE_URL,
+	base: BASE_URL,
 	integrations: [tailwind()],
 	i18n: {
 		defaultLocale: 'es',
