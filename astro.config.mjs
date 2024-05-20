6f64d50871b97c60153b6e7342dac72aa9fac731
checomendoza <ezequiel.mendoza@gmail.com>
2024-05-20 17:41:56 -0300

    Config GithubPages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://checomendoza.github.io',
+	base: 'ezequielmendoza.dev',
 	integrations: [tailwind()],
 	i18n: {
 		defaultLocale: 'es',
