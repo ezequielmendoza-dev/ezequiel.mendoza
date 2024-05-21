d232b3d2d571dda74a150f43f1307558c76fdd9e
checomendoza <ezequiel.mendoza@gmail.com>
2024-05-21 07:37:35 -0300

    Change links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,14 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 const SERVER_PORT = 4321
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`
-const PRODUCTION_URL = 'https://checomendoza.github.io'
+const PRODUCTION_HOST_URL = 'https://checomendoza.github.io'
 let SITE_URL = LOCALHOST_URL
 let BASE_URL = ''
 
 const SCRIPT = process.env.npm_lifecycle_script || ''
 const isBuild = SCRIPT.includes('astro build')
 if (isBuild) {
-	SITE_URL = PRODUCTION_URL
+	SITE_URL = PRODUCTION_HOST_URL
 	BASE_URL = '/ezequiel.mendoza'
 }
 // https://astro.build/config
