517effd9a5f4541da70ad8fe2ffb93f623513ed0
checomendoza <ezequiel.mendoza@gmail.com>
2024-05-23 08:41:28 -0300

    Add custom font Redit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				redit: ['"Reddit Sans"', 'sans'],
+			},
+		},
 	},
 	plugins: [],
 }
