f9a45af8bae54339072df6e5f01042230eea6f09
checomendoza <ezequiel.mendoza@gmail.com>
2024-08-29 07:37:47 -0300

    Add partytown to Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,42 @@
 /** @format */
 
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind'
-const SERVER_PORT = 4321
-const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`
-const PRODUCTION_HOST_URL = 'https://checomendoza.github.io'
-let SITE_URL = LOCALHOST_URL
-let BASE_URL = ''
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+const SERVER_PORT = 4321;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const PRODUCTION_HOST_URL = "https://checomendoza.github.io";
+let SITE_URL = LOCALHOST_URL;
+let BASE_URL = "";
 
-const SCRIPT = process.env.npm_lifecycle_script || ''
-const isBuild = SCRIPT.includes('astro build')
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
 if (isBuild) {
-	SITE_URL = PRODUCTION_HOST_URL
-	BASE_URL = '/ezequiel.mendoza'
+  SITE_URL = PRODUCTION_HOST_URL;
+  BASE_URL = "/ezequiel.mendoza";
 }
 // https://astro.build/config
 export default defineConfig({
-	site: SITE_URL,
-	base: BASE_URL,
-	integrations: [tailwind()],
-	i18n: {
-		defaultLocale: 'es',
-		locales: ['es', 'en'],
-		routing: {
-			prefixDefaultLocale: false,
-		},
-	},
-})
+  site: SITE_URL,
+  base: BASE_URL,
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+});
